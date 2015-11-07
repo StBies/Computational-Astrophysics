68541a184a32f3904908cb6c6a213460a72ca892
Stefan <Dinge_passieren@web.de>
2015-11-07 17:59:24 +0100

    Removed documentation from repository

--- Problemset1/Exercise2/doc/html/search/all_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['getmatrix',['getMatrix',['../classMatrix.html#a4f142b8c81336b5f4c494ae7faa1caac',1,'Matrix']]],
-  ['getsize',['getSize',['../classMatrix.html#ad6810e0bc4061dd3492a99aa60107018',1,'Matrix']]],
-  ['getvalue',['getValue',['../classMatrix.html#ac61c1914809e815b008d2ad55473a982',1,'Matrix']]]
-];
