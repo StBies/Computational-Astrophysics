63b56c62cbd5050665075fd96983f07a0da986cc
Stefan <Dinge_passieren@web.de>
2015-11-07 18:05:14 +0100

    Removed documentation

--- Problemset1/Exercise2/doc/html/search/functions_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['matrix',['Matrix',['../classMatrix.html#a2dba13c45127354c9f75ef576f49269b',1,'Matrix::Matrix()'],['../classMatrix.html#a0c36e51b0557906e18874b449084c922',1,'Matrix::Matrix(int size, int **matrix)']]],
-  ['multiply',['multiply',['../classMatrix.html#a7f0688366fee2056c0750f6dd401c344',1,'Matrix']]]
-];
--- Problemset1/Exercise2/doc/html/search/functions_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['setmatrix',['setMatrix',['../classMatrix.html#a17b0e0d2d2eed781cc346d27388a6ddc',1,'Matrix']]],
-  ['setsize',['setSize',['../classMatrix.html#a81229f23ae174ff8c3cb9cb6c41ff87d',1,'Matrix']]],
-  ['setvalue',['setValue',['../classMatrix.html#a1cc6730760fc5417d080d68a31c30436',1,'Matrix']]]
-];
