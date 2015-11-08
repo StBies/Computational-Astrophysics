8c5f9f8050b6cd58f88b0d362955ee4899b250a8
Stefan <Dinge_passieren@web.de>
2015-11-08 12:26:37 +0100

    Added working multiplication functionality for stdandard c arrays in main.cpp Passing of the resulting object in Matrix class to caller doesn't work yet

--- Problemset1/Exercise2/main.cpp
+++ Problemset1/Exercise2/main.cpp
@@ -6,39 +6,45 @@ using namespace std;
 
 void fill(int size, int** a, int** b);
 
+int** multiply(int size, int** a, int** b);
+
 int main()
 {
 	int size = 2;
 	int** a;
 	int** b;
+	int** c;
 
 	a = new int*[size];
 	b = new int*[size];
+	c = new int*[size];
 
 	for (int i = 0; i < size; i++)
 	{
 		a[i] = new int[size];
 		b[i] = new int[size];
+		c[i] = new int[size];
 	}
 
-	fill(size,a,b);
+	fill(size, a, b);
 
-	Matrix* A = new Matrix(size,a);
-	Matrix* B = new Matrix(size,b);
+	Matrix* A = new Matrix(size, a);
+	Matrix* B = new Matrix(size, b);
 	Matrix* C = A->multiply(*B);
 
-	cout << *C[1][1] << endl;
+	c = multiply(size,a,b);
 
+	cout << c[1][1] << endl;
 
 	return 0;
 }
 
 void fill(int size, int** a, int** b)
 {
-	for(int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		int j;
-		for(j = 0; j < i; j++)
+		for (j = 0; j < i; j++)
 		{
 			a[i][j] = 5;
 			b[i][j] = 3;
@@ -47,10 +53,38 @@ void fill(int size, int** a, int** b)
 		a[i][j] = 2;
 		b[i][j] = 1;
 
-		for(j++; j < size; j++)
+		for (j++; j < size; j++)
 		{
 			a[i][j] = 4;
 			b[i][j] = 6;
 		}
 	}
+}
+
+int** multiply(int size, int** a, int** b)
+{
+	int** result;
+	result = new int*[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		result[i] = new int[size];
+	}
+
+	int stepresult = 0;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			for (int k = 0; k < size; k++)
+			{
+
+				stepresult += a[i][k] * b[k][j];
+			}
+			result[i][j] = stepresult;
+			stepresult = 0;
+		}
+	}
+
+	return result;
 }
