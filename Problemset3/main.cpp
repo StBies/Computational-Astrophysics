7c267bb2b1d962f921998bda95dfa370bab89dbe
Stefan <Dinge_passieren@web.de>
2015-12-07 12:18:41 +0100

    Added Jupiterlike planet to the simulation

--- Problemset3/main.cpp
+++ Problemset3/main.cpp
@@ -11,15 +11,21 @@ int main()
 {
 	CelBody* star = new Star(SOLAR_MASS,0,0,0,0);
 	CelBody* planet = new Planet(EARTH_MASS,AU,0,0,-EARTH_ORBIT_VELOCITY);
+	CelBody* planet2 = new Planet(JUPITER_MASS,13.6*AU,0,0,-JUPITER_ORBIT_VELOCITY);
 	PhysSim* sim = new PhysSim();
 	sim->addBody(star);
 	sim->addBody(planet);
+	sim->addBody(planet2);
 
 	//simulate orbit for 2 years, one day step
 	for(int i = 0; i < 2 * 365; i++)
 	{
 		sim->update(YEAR / 365);
-		cout << planet->getX() / AU << "\t" << planet->getY() / AU << "\t" << planet->getEnergy() << endl;
+		cout << planet->getX() / AU << "\t"
+			 << planet->getY() / AU << "\t"
+			 << planet2->getX() / AU << "\t"
+			 << planet2->getY() / AU
+			 << endl;
 	}
 
 	delete sim;
