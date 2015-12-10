14e70504cfae550567015a57e3c60518f6e4f905
Stefan <Dinge_passieren@web.de>
2015-12-10 15:47:30 +0100

    Added easiest MC integrator without error estimation, added Makefile, started commenting

--- Problemset4/main.c
+++ Problemset4/main.c
@@ -8,15 +8,50 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+bool isUnder(double x, double y);
 
 int main(void)
 {
-	int x = 0;
+	double x = 0;
+	double y = 0;
+
 	srand(1337);
-	for(int i = 0; i < 1e6; i++)
+
+	int N = 1e6;
+	int counter = 0;
+	for(int i = 0; i < N; i++)
 	{
-		x = rand();
-		printf("%d\n",x);
+		x = rand() / (double)RAND_MAX;
+		y = rand() / (double)RAND_MAX;
+
+		if(isUnder(x,y))
+		{
+			counter++;
+		}
 	}
+
+	printf("Real integral is about: 0.78540, this calculation: %f\n", (double)counter/N);
+
 	return 0;
+
 }
+
+/**
+ * Checks, if the value y is smaller than the value of the function sqrt(1-x**2) at point x
+ *
+ * @author Stefan
+ * @date Dec. 10, 2015
+ * @version 0.1
+ *
+ * @param x point, at which the function is to be evaluated
+ * @param y value that is to be checked if smaller than the value of the function
+ *
+ * @return true if y is smaller or equal to sqrt(1-x**2), else false
+ */
+bool isUnder(double x, double y)
+{
+	return y <= sqrt(1.0 - pow(x,2.0));
+}
+
