5806b322574e514a54c85a137afcf44948916dd2
Stefan <Dinge_passieren@web.de>
2016-01-18 22:41:29 +0100

    Created Doc-commentary, not good yet

--- Problemset5/Data.cpp
+++ Problemset5/Data.cpp
@@ -9,6 +9,15 @@
 
 using namespace std;
 
+/**
+ * Constructor, that reads a datafile and stores it in real data.
+ *
+ * @author Stefan
+ * @date Jan. 18, 2016
+ * @version 0.1
+ *
+ * @param file A string giving the filename with path if needed to the datafile
+ */
 Data::Data(string file)
 {
 	_rawData = new vector<string>;
@@ -33,17 +42,36 @@ Data::Data(string file)
 	}
 }
 
+/**
+ * Destructor, cleaning memory.
+ *
+ * @author Stefan
+ * @date Jan. 18, 2016
+ * @version 0.1
+ */
 Data::~Data()
 {
-	// TODO Auto-generated destructor stub
+	delete [] _fluxLog;
+	delete [] _wavelength;
+	delete [] _rawData;
 }
 
+/**
+ * A parser, that gets real Data out of the textfile containing it using regular expressions.
+ *
+ * @author Stefan
+ * @date Jan. 18, 2016
+ * @version 0.1
+ */
 void Data::parseLine()
 {
+	//regular expression for more than one digit, maybe a dot and maybe some more digits
 	regex number("(\\d)+\\.(?\\d*)");
 	smatch submatch;
+
 	for(string line: *_rawData)
 	{
+		//TODO compilationtime error here, fix it
 		regex_match matcher(line.c_str(),submatch,number);
 	}
 }
