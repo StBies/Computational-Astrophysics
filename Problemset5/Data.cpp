4054290e1b64bec2988942d406ba90b3120ab326
Stefan <Dinge_passieren@web.de>
2016-01-19 10:32:40 +0100

    Rejected use of regular expressions, used stringstream instead, edited Makefile, more minor changes

--- Problemset5/Data.cpp
+++ Problemset5/Data.cpp
@@ -18,28 +18,27 @@ using namespace std;
  *
  * @param file A string giving the filename with path if needed to the datafile
  */
-Data::Data(string file)
+Data::Data(const string file)
 {
 	_rawData = new vector<string>;
-	_wavelength = new vector<double>;
-	_fluxLog = new vector<double>;
+	_data = new vector<vector<double>*>;
 
 	ifstream datafile;
 	datafile.open(file.c_str());
 
-	while(!datafile.eof())
+	cout << "Reading data from file: " << file << endl;
+
+	while (!datafile.eof())
 	{
 		string s;
-		getline(datafile,s);
+		getline(datafile, s);
+		parseLine(s);
 		_rawData->push_back(s);
 	}
-
 	datafile.close();
+	cout << "Done reading " << file << endl;
 
-	for(string s: *_rawData)
-	{
-		cout << s << endl;
-	}
+	_data->shrink_to_fit();
 }
 
 /**
@@ -51,28 +50,46 @@ Data::Data(string file)
  */
 Data::~Data()
 {
-	delete [] _fluxLog;
-	delete [] _wavelength;
-	delete [] _rawData;
+	delete[] _rawData;
 }
 
 /**
- * A parser, that gets real Data out of the textfile containing it using regular expressions.
+ * Getter-method for the data, the object holds. Will return a pointer to a vector containing
+ * pointers to double-vectors.
+ *
+ * @author Stefan
+ * @date Jan. 19, 2016
+ * @version 0.1
+ *
+ * @return Pointer to a vector of pointers to vectors of doubles
+ */
+vector<vector<double>*>* Data::getData()
+{
+	return _data;
+}
+
+/**
+ * A parser, that gets real Data out of the textfile containing it.
  *
  * @author Stefan
  * @date Jan. 18, 2016
  * @version 0.1
+ *
+ * @param line A String containing the line to be parsed
  */
-void Data::parseLine()
+void Data::parseLine(const string line)
 {
-	//regular expression for more than one digit, maybe a dot and maybe some more digits
-	regex number("(\\d+)\\.(?\\d*)");
-	smatch submatch;
+	stringstream inStream;
+	inStream << line;
+	double a = 0;
+	double b = 0;
 
-	for(string line: *_rawData)
-	{
-		//TODO compilationtime error here, fix it
-		regex_match matcher(line.c_str(),submatch,number);
-	}
+	vector<double>* content = new vector<double>;
+	content->resize(2);
+
+	inStream >> a >> b;
+	content->push_back(a);
+	content->push_back(b);
+	_data->push_back(content);
 }
 
--- Problemset5/Data.h
+++ Problemset5/Data.h
@@ -12,19 +12,30 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <regex>
+#include <sstream>
 
+using namespace std;
+
+/**
+ * Class containing data of a solar spectrum, especially wavelength and a corresponding
+ * flux for each wavelength
+ *
+ * @author Stefan
+ * @date Jan. 19, 2016
+ * @version 0.3
+ */
 class Data
 {
 public:
-	Data(std::string file);
+	Data(const string file);
 	virtual ~Data();
+	vector<vector<double>*>* getData();
+
 
 private:
-	void parseLine();
-	std::vector<std::string>* _rawData;
-	std::vector<double>* _wavelength;
-	std::vector<double>* _fluxLog;
+	void parseLine(const string line);
+	vector<vector<double>*>* _data;
+	vector<string>* _rawData;
 };
 
 #endif /* DATA_H_ */
